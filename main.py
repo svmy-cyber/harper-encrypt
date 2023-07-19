db6a7613d8bff73f8df0e295d4b2b43ddc4d7f59
Sam Young <sam@svmy.io>
2023-07-19 11:19:40 +1000

    Finished decryption character block

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def save_to_file(text, filename):
         file.write(text)
     return filename
 
+
 class EncryptedCharacterContainer:
     def __init__(self, public_key, character):
         assert isinstance(public_key, PublicKey)
@@ -37,14 +38,39 @@ class StandardEquation:
         assert isinstance(constant, int)
         self.coefficients = coefficients
         self.constant = constant
+
     def embed_data(self, data):
         assert isinstance(data, int)
         self.constant = self.constant + data
+
     def add_equation(self, standard_equation):
         assert isinstance(standard_equation, StandardEquation)
         for index, coefficient in enumerate(standard_equation.coefficients):
             self.coefficients[index] = self.coefficients[index] + coefficient
         self.constant = self.constant + standard_equation.constant
+
+    def extract_data(self, vectors, mod_value):
+        assert isinstance(vectors, list)
+        assert isinstance(mod_value, int)
+        tolerance = (mod_value // 4)
+        affirmative = mod_value // 2
+        affirmative_lower_boundary = affirmative - tolerance
+        affirmative_upper_boundary = affirmative + tolerance
+        negative = 0
+        negative_lower_boundary = negative - tolerance
+        negative_upper_boundary = negative + tolerance
+        actual_solution = 0
+        for index, vector in vectors:
+            product = vector * self.coefficients[index]
+            actual_solution = actual_solution + product
+        difference = self.constant - actual_solution
+        if affirmative_upper_boundary > difference > affirmative_lower_boundary:
+            data = 1
+        elif negative_upper_boundary > difference > negative_lower_boundary:
+            data = 0
+        else:
+            raise Exception("Invalid data detected") # replace with random generated data later
+        return data
 
 
 class EncapsulationEquation:
@@ -59,7 +85,6 @@ class EncapsulationEquation:
             self.encapsulation_equation.add_equation(equation)
         self.processed_data = ((public_key.private_key.mod_value // 2) * data)
         self.encapsulation_equation.embed_data(self.processed_data)
-
 
 
 class EncryptedBlock:
@@ -84,11 +109,10 @@ class DecryptedBlock:
         self.private_key = private_key
         self.encrypted_string = encrypted_string
         self.encrypted_object = list(encrypted_string)
-        self.decrypted_object = []
         self.plain_string = ""
         for character_encrypted in self.encrypted_object:
             character_decrypted = DecryptedCharacterContainer(self.public_key, self.private_key, character_encrypted)
-
+            self.plain_string = self.plain_string + character_decrypted.character_ascii_char
 
 
 class DecryptedCharacterContainer:
@@ -97,13 +121,17 @@ class DecryptedCharacterContainer:
         assert isinstance(private_key, PrivateKey)
         assert isinstance(character_encrypted, list)
         self.public_key = public_key
+        self.private_key = private_key
         self.character_encrypted = character_encrypted
         self.character_binary_string = ""
-        for equation in character_encrypted:
-
+        for equation_string in character_encrypted:
+            equation_list = list(equation_string)
+            equation_coefficient_list = list(equation_list[0])
+            equation_constant = equation_list[1]
+            equation = StandardEquation(equation_coefficient_list, equation_constant)
+            self.character_binary_string = self.character_binary_string + equation.extract_data(self.private_key.vectors, self.private_key.mod_value)
         self.character_unicode_int = int(self.character_binary_string, 2)
         self.character_ascii_char = chr(self.character_unicode_int)
-
 
 
 class PrivateKey:
@@ -133,17 +161,19 @@ class PublicKey:
             self.standard_equations.append(equation)
 
 
-
-
-
 def select_random_equation(public_key: PublicKey):
+    index = return_random_int(public_key.private_key.mod_value)
+    return public_key.standard_equations[index]
+
+
+def return_random_int(mod_value: int):
     nanoseconds = perf_counter_ns()
     factor = 1
     for digit in str(nanoseconds):
         if int(digit) != 0:
             factor = (factor + int(digit)) * int(digit)
-    factor = factor % public_key.private_key.mod_value
-    return public_key.standard_equations[factor]
+    factor = factor % mod_value
+    return factor
 
 
 def show_menu():
