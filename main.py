1e7d94c275cf370ac4c5756b41f1b43176a6668e
Sam Young <sam@svmy.io>
2023-07-19 10:40:09 +1000

    Making progress building up the encapsulation classes

--- main.py
+++ main.py
@@ -16,13 +16,20 @@ def save_to_file(text, filename):
         file.write(text)
     return filename
 
-class CharacterContainer:
-    def __init__(self, character):
+class EncryptedCharacterContainer:
+    def __init__(self, public_key, character):
+        assert isinstance(public_key, PublicKey)
         assert isinstance(character, str)
+        self.public_key = public_key
         self.character_ascii_char = character
         self.character_unicode_int = ord(self.character_ascii_char)[2:]
         self.character_binary_int = bin(self.character_unicode_int)
         self.character_binary_string = str(self.character_binary_int)
+        self.character_encrypted = []
+        for binary_character in self.character_binary_string:
+            encapsulation_equation = EncapsulationEquation(self.public_key, binary_character == "1")
+            self.character_encrypted.append(encapsulation_equation)
+
 
 class StandardEquation:
     def __init__(self, coefficients, constant):
@@ -30,11 +37,73 @@ class StandardEquation:
         assert isinstance(constant, int)
         self.coefficients = coefficients
         self.constant = constant
+    def embed_data(self, data):
+        assert isinstance(data, int)
+        self.constant = self.constant + data
+    def add_equation(self, standard_equation):
+        assert isinstance(standard_equation, StandardEquation)
+        for index, coefficient in enumerate(standard_equation.coefficients):
+            self.coefficients[index] = self.coefficients[index] + coefficient
+        self.constant = self.constant + standard_equation.constant
 
-    def produce_blueprint(self):
-        coefficients = tuple(self.coefficients)
-        constant = self.constant
-        return coefficients, constant
+
+class EncapsulationEquation:
+    def __init__(self, public_key, data):
+        assert isinstance(public_key, PublicKey)
+        self.component_standard_equations = []
+        self.encapsulation_equation = StandardEquation([], 0)
+        self.component_limit = public_key.private_key.mod_value // 23
+        for component in range(self.component_limit):
+            self.component_standard_equations.append(select_random_equation(public_key))
+        for equation in self.component_standard_equations:
+            self.encapsulation_equation.add_equation(equation)
+        self.processed_data = ((public_key.private_key.mod_value // 2) * data)
+        self.encapsulation_equation.embed_data(self.processed_data)
+
+
+
+class EncryptedBlock:
+    def __init__(self, public_key, plain_string):
+        assert isinstance(public_key, PublicKey)
+        assert isinstance(plain_string, str)
+        self.public_key = public_key
+        self.plain_string = plain_string
+        self.encrypted_object = []
+        self.encrypted_string = ""
+        for letter in plain_string:
+            encrypted_character = EncryptedCharacterContainer(self.public_key, letter)
+            self.encrypted_object.append(encrypted_character.character_encrypted)
+
+
+class DecryptedBlock:
+    def __init__(self, public_key, private_key, encrypted_string):
+        assert isinstance(public_key, PublicKey)
+        assert isinstance(private_key, PrivateKey)
+        assert isinstance(encrypted_string, str)
+        self.public_key = public_key
+        self.private_key = private_key
+        self.encrypted_string = encrypted_string
+        self.encrypted_object = list(encrypted_string)
+        self.decrypted_object = []
+        self.plain_string = ""
+        for character_encrypted in self.encrypted_object:
+            character_decrypted = DecryptedCharacterContainer(self.public_key, self.private_key, character_encrypted)
+
+
+
+class DecryptedCharacterContainer:
+    def __init__(self, public_key, private_key, character_encrypted):
+        assert isinstance(public_key, PublicKey)
+        assert isinstance(private_key, PrivateKey)
+        assert isinstance(character_encrypted, list)
+        self.public_key = public_key
+        self.character_encrypted = character_encrypted
+        self.character_binary_string = ""
+        for equation in character_encrypted:
+
+        self.character_unicode_int = int(self.character_binary_string, 2)
+        self.character_ascii_char = chr(self.character_unicode_int)
+
 
 
 class PrivateKey:
@@ -62,19 +131,9 @@ class PublicKey:
             constant = 45 + private_key.errors[index]
             equation = StandardEquation(coefficients, constant)
             self.standard_equations.append(equation)
-    def produce_blueprint(self):
-            # turn the standard_equations list into a tuple
 
 
-class EncapsulationEquation:
-    def __init__(self, public_key):
-        assert isinstance(public_key, PublicKey)
-        self.component_standard_equations = []
-        self.coefficients = []
-        self.constant = 0
-        self.component_limit = public_key.private_key.mod_value // 23
-        for component in range(self.component_limit):
-            self.component_standard_equations.append(select_random_equation(public_key))
+
 
 
 def select_random_equation(public_key: PublicKey):
@@ -86,6 +145,37 @@ def select_random_equation(public_key: PublicKey):
     factor = factor % public_key.private_key.mod_value
     return public_key.standard_equations[factor]
 
+
+def show_menu():
+    print("1. Configure New Key Pair")
+    print("2. Encrypt Text")
+    print("3. Decrypt Text")
+    print("4. Exit")
+
+
+def handle_option(selected_option):
+    if selected_option == 1:
+        print("Configure Key Pair")
+
+    elif selected_option == 2:
+        print("Encrypt Text")
+
+    elif selected_option == 3:
+        print("Decrypt Text")
+
+    elif selected_option == 4:
+        print("Exiting the program...")
+        exit()
+    else:
+        print("Invalid option. Please try again.")
+
 if __name__ == '__main__':
 
-
+    while True:
+        show_menu()
+        user_input = input("Select an option: ")
+        try:
+            option = int(user_input)
+            handle_option(option)
+        except ValueError:
+            print("Invalid input. Please enter a number.")
